ab9eda0ac8143fa2e2494dc4d8b3e1c33b97d7ed
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-10 18:20:37 -0500

    Added a command line option parser.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,70 @@ mod markdown;
 
 use markdown::Markdown;
 
+struct CmdOptions {
+    input: Option<String>,
+    output: String,
+    template: Option<String>
+}
+
+fn get_cmd_options() -> CmdOptions {
+    let mut options = CmdOptions {
+        input: None,
+        output: "index.html".to_string(),
+        template: None
+    };
+    
+    // This states what the next argument will be.
+    #[derive(Copy, Clone)]
+    enum NextArgument {
+        Input, Output, Template
+    }
+    
+    let mut next_arg = NextArgument::Input;
+    
+    let mut args = std::env::args().collect::<Vec<String>>();
+    args.remove(0); // The first argument is the program name.
+    
+    args.iter().for_each(|argument| {
+        match argument.as_str() {
+            "-o" => next_arg = NextArgument::Output,
+            "-t" => next_arg = NextArgument::Template,
+            "--output" => next_arg = NextArgument::Output,
+            "--template" => next_arg = NextArgument::Template,
+            _ => match next_arg {
+                NextArgument::Input => options.input = Some(argument.clone()),
+                NextArgument::Output => {
+                    options.output = argument.clone();
+                    next_arg = NextArgument::Input;
+                },
+                NextArgument::Template => {
+                    options.template = Some(argument.clone());
+                    next_arg = NextArgument::Input;
+                }
+            }
+        }
+    });
+    
+    options
+}
+
 fn main() {
     let markdown = Markdown::from_file("misc/test_file.md").unwrap();
+    let options = get_cmd_options();
+    
+    if options.input.is_none() {
+        println!("Please specify an input file.");
+        return;
+    }
+    
+    let input = options.input.unwrap();
     
     println!("{}", markdown.to_html());
+    
+    println!("The input file: {}", input);
+    println!("The output file: {}", options.output);
+    
+    if let Some(template) = options.template {
+        println!("The template file: {}", template);
+    }
 }
