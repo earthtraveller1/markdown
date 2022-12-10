06af39631646dbe6416e98f158d0aab78cc8c853
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-10 17:13:52 -0500

    Added a method to construct a Markdown object from a file.

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
 // A basic file containing the components required for parsing Markdown files.
 
 enum ElementType {
@@ -11,4 +13,48 @@ struct Element {
 
 struct Markdown {
     elements: Vec<Element>
+}
+
+enum MarkdownError {
+    FileOpeningError
+}
+
+impl Markdown {
+    fn from_file(file_name: &str) -> Result<Markdown, MarkdownError> {
+        let file = File::open(file_name);
+        if let Ok(file) = file {
+            let reader = BufReader::new(file);
+            Ok(
+                Markdown {
+                    elements: reader.lines().map(|line| {
+                        let line = line.unwrap();
+                        
+                        if line.starts_with("# ") {
+                            Element {
+                                element_type: ElementType::Header1,
+                                text: line.strip_prefix("# ").unwrap().to_string()
+                            }
+                        } else if line.starts_with("## ") {
+                            Element {
+                                element_type: ElementType::Header2,
+                                text: line.strip_prefix("## ").unwrap().to_string()
+                            }
+                        } else if line.starts_with("### ") {
+                            Element {
+                                element_type: ElementType::Header3,
+                                text: line.strip_prefix("### ").unwrap().to_string()
+                            }
+                        } else {
+                            Element {
+                                element_type: ElementType::Paragraph,
+                                text: line.clone()
+                            }
+                        }
+                    }).collect()
+                }
+            )
+        } else {
+            Err(MarkdownError::FileOpeningError)
+        }
+    }
 }
