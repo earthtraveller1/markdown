7b81b89ce2c57e01eabad1cbdaa1595c83054d77
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-10 17:19:04 -0500

    A bit of refractoring.

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,5 +1,3 @@
-use std::{fs::File, io::{BufRead, BufReader}};
-
 // A basic file containing the components required for parsing Markdown files.
 
 enum ElementType {
@@ -16,45 +14,44 @@ struct Markdown {
 }
 
 enum MarkdownError {
-    FileOpeningError
+    FileReadError
 }
 
 impl Markdown {
     fn from_file(file_name: &str) -> Result<Markdown, MarkdownError> {
-        let file = File::open(file_name);
-        if let Ok(file) = file {
-            let reader = BufReader::new(file);
-            Ok(
-                Markdown {
-                    elements: reader.lines().map(|line| {
-                        let line = line.unwrap();
-                        
-                        if line.starts_with("# ") {
-                            Element {
-                                element_type: ElementType::Header1,
-                                text: line.strip_prefix("# ").unwrap().to_string()
-                            }
-                        } else if line.starts_with("## ") {
-                            Element {
-                                element_type: ElementType::Header2,
-                                text: line.strip_prefix("## ").unwrap().to_string()
-                            }
-                        } else if line.starts_with("### ") {
-                            Element {
-                                element_type: ElementType::Header3,
-                                text: line.strip_prefix("### ").unwrap().to_string()
-                            }
-                        } else {
-                            Element {
-                                element_type: ElementType::Paragraph,
-                                text: line.clone()
-                            }
-                        }
-                    }).collect()
-                }
-            )
+        let contents = std::fs::read_to_string(file_name);
+        if let Ok(contents) = contents {
+            Ok(Markdown::from_str(contents.as_str()))
         } else {
-            Err(MarkdownError::FileOpeningError)
+            Err(MarkdownError::FileReadError)
+        }
+    }
+    
+    fn from_str(contents: &str) -> Markdown {
+        Markdown {
+            elements: contents.lines().map(|line| {
+                if line.starts_with("# ") {
+                    Element {
+                        element_type: ElementType::Header1,
+                        text: line.strip_prefix("# ").unwrap().to_string()
+                    }
+                } else if line.starts_with("## ") {
+                    Element {
+                        element_type: ElementType::Header2,
+                        text: line.strip_prefix("## ").unwrap().to_string()
+                    }
+                } else if line.starts_with("### ") {
+                    Element {
+                        element_type: ElementType::Header3,
+                        text: line.strip_prefix("### ").unwrap().to_string()
+                    }
+                } else {
+                    Element {
+                        element_type: ElementType::Paragraph,
+                        text: line.to_string()
+                    }
+                }
+            }).collect()
         }
     }
 }
