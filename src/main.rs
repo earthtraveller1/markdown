1754a0f7a30f9688be99b87431ec15206323780c
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-27 16:33:30 -0400

    Made Input and Output different types.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-enum InputOutput {
+enum Input {
     File(std::fs::File),
-    Standard,
+    Stdin,
+}
+
+enum Output {
+    File(std::fs::File),
+    Stdout,
 }
 
 struct CmdOptions {
-    input: InputOutput,
-    output: InputOutput,
+    input: Input,
+    output: Output,
 }
 
 impl CmdOptions {
     fn get() -> Result<CmdOptions, ()> {
         let mut cmd_args = std::env::args();
         let mut cmd_options = CmdOptions {
-            input: InputOutput::Standard,
-            output: InputOutput::Standard,
+            input: Input::Stdin,
+            output: Output::Stdout,
         };
 
         while let Some(arg) = cmd_args.next() {
             if arg == "-o" || arg == "--output" {
                 match cmd_args.next() {
                     Some(path) => {
-                        cmd_options.output = InputOutput::File({
+                        cmd_options.output = Output::File({
                             let file = std::fs::File::create(path.as_str());
                             match file {
                                 Ok(file) => file,
@@ -39,7 +44,7 @@ impl CmdOptions {
             } else {
                 cmd_options.input = {
                     match std::fs::File::open(arg.as_str()) {
-                        Ok(file) => InputOutput::File(file),
+                        Ok(file) => Input::File(file),
                         Err(_) => {
                             eprintln!("[ERROR]: Could not open '{}'", arg);
                             return Err(());
