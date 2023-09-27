5d1afefa0740cc9b3e4db100aceaaca9c0d7b003
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-27 16:38:51 -0400

    Implemented functions for reading and writing to the inputs and outputs.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+use std::io::{Read, Write};
+
 enum Input {
     File(std::fs::File),
     Stdin,
 }
 
+impl Input {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
+        match self {
+            Input::File(file) => file.read(buf),
+            Input::Stdin => std::io::stdin().read(buf),
+        }
+    }
+}
+
 enum Output {
     File(std::fs::File),
     Stdout,
+}
+
+impl Output {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        match self {
+            Output::File(file) => file.write(buf),
+            Output::Stdout => std::io::stdout().write(buf),
+        }
+    }
 }
 
 struct CmdOptions {
