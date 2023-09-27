2b6e509410b30e541668057a78382e432cdfbad4
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-27 16:25:58 -0400

    Added a basic command line parser for recieving files.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+enum InputOutput {
+    File(std::fs::File),
+    Standard,
 }
+
+struct CmdOptions {
+    input: InputOutput,
+    output: InputOutput,
+}
+
+impl CmdOptions {
+    fn get() -> Result<CmdOptions, ()> {
+        let mut cmd_args = std::env::args();
+        let mut cmd_options = CmdOptions {
+            input: InputOutput::Standard,
+            output: InputOutput::Standard,
+        };
+
+        while let Some(arg) = cmd_args.next() {
+            if arg == "-o" || arg == "--output" {
+                match cmd_args.next() {
+                    Some(path) => {
+                        cmd_options.output = InputOutput::File({
+                            let file = std::fs::File::create(path.as_str());
+                            match file {
+                                Ok(file) => file,
+                                Err(_) => {
+                                    eprintln!("[ERROR]: Could not open '{}' for writing.", path);
+                                    return Err(());
+                                }
+                            }
+                        });
+                    }
+                    None => {
+                        eprintln!("[ERROR]: Expected filename after '{}'", arg);
+                        return Err(());
+                    }
+                }
+            } else {
+                cmd_options.input = {
+                    match std::fs::File::open(arg.as_str()) {
+                        Ok(file) => InputOutput::File(file),
+                        Err(_) => {
+                            eprintln!("[ERROR]: Could not open '{}'", arg);
+                            return Err(());
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(cmd_options)
+    }
+}
+
+fn main() {}
