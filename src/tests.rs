d279a6fc83ce8d76366dace52247ed13f53ddd6d
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-10-05 17:43:35 -0400

    Changed the tests so that they pass.

--- src/tests.rs
+++ src/tests.rs
@@ -16,9 +16,9 @@ Regular Paragraph
         .map(|paragraph| crate::Paragraph::parse(paragraph).unwrap())
         .collect::<Vec<crate::Paragraph>>();
 
-    assert_eq!(paragraphs[0].render_html(), "<h1>Title</h1>");
-    assert_eq!(paragraphs[1].render_html(), "<h2>Second Title</h2>");
-    assert_eq!(paragraphs[2].render_html(), "<h3>Third Title</h3>");
+    assert_eq!(paragraphs[0].render_html(), "<p><h1>Title</h1></p>");
+    assert_eq!(paragraphs[1].render_html(), "<p><h2>Second Title</h2></p>");
+    assert_eq!(paragraphs[2].render_html(), "<p><h3>Third Title</h3></p>");
     assert_eq!(paragraphs[3].render_html(), "<p>Regular Paragraph</p>");
 }
 
